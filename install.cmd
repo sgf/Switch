67821918a4f41e8655e24ee676bd1b21e14fa2ca
Yves Fiumefreddo <y.fiumefreddo@gmail.com>
2018-02-24 11:39:39 +0100

    change version to 0.6.3

--- install.cmd
+++ install.cmd
@@ -1,2 +1,2 @@
-@set switch_version=0.6.2
+@set switch_version=0.6.3
 @call scripts\install\install.cmd %*
--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-export switch_version=0.6.2
+export switch_version=0.6.3
 scripts/install/install.sh "$@"
