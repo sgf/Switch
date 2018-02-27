1758c0cb8d01d90225b3d50b48c961ccec969f1a
gammasoft71 <gammasoft71@gmail.com>
2018-02-27 18:10:57 +0100

    enable coverage

--- scripts/travis/build_macos.sh
+++ scripts/travis/build_macos.sh
@@ -3,8 +3,7 @@
 # ______________________________________________________________________________
 #                                             generate, build and install Switch
 pushd build
-# ENABLE_COVERAGE deactivated if not the job exceeds the maximum time limit.
-cmake .. -DCMAKE_BUILD_TYPE=Debug -DBUILD_SWITCH_TESTS=ON -DENABLE_COVERAGE=OFF -DCMAKE_INSTALL_PREFIX=~/usr/local
+cmake .. -DCMAKE_BUILD_TYPE=Debug -DBUILD_SWITCH_TESTS=ON -DENABLE_COVERAGE=ON -DCMAKE_INSTALL_PREFIX=~/usr/local
 cmake --build . -- -j8
 popd
 
