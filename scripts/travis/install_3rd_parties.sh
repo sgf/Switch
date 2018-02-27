eaba588a48b8be5d6c6f7f8cac9c234bed8ed6e2
gammasoft71 <gammasoft71@gmail.com>
2018-02-27 13:10:11 +0100

    review scripts

--- scripts/travis/install_3rd_parties.sh
+++ scripts/travis/install_3rd_parties.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-set -ev
-
 # ________________________________________________________________________________________
 #                                                                      install 3rd parties
 mkdir -p build/3rdparty
 pushd build
 
-if [[ "${TRAVIS_OS_NAME}" == "linux" ]]; then
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   mkdir -p external/cmake
   pushd external/cmake
   wget https://cmake.org/files/v3.8/cmake-3.8.0-Linux-x86_64.sh
@@ -15,8 +13,11 @@ if [[ "${TRAVIS_OS_NAME}" == "linux" ]]; then
   ./cmake-*-Linux-x86_64.sh --exclude-subdir --skip-license
   export PATH="${PWD}/bin:$PATH"
   popd
-else
+fi
+
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
   brew update
   brew install astyle cppcheck curl jpeg libpng zlib -y
 fi
+
 popd
