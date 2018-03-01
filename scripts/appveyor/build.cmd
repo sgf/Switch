11fd32466d2ecf1107a687e873e5417d57a13bad
gammasoft71 <gammasoft71@gmail.com>
2018-03-01 14:05:21 +0100

    add exit code to appveyor

--- scripts/appveyor/build.cmd
+++ scripts/appveyor/build.cmd
@@ -4,4 +4,5 @@ mkdir build
 cd build
 cmake -G "%BUILD_OPTION%" .. -DBUILD_SWITCH_TESTS=ON
 cmake --build . --config Debug
+if %ERRORLEVEL% NEQ 0 exit 1
 cd ..
--- scripts/appveyor/test.cmd
+++ scripts/appveyor/test.cmd
@@ -2,4 +2,5 @@ rem ____________________________________________________________________________
 rem                                                    run registered unit tests
 cd build
 ctest --output-on-failure --build-config Debug
+if %ERRORLEVEL% NEQ 0 exit 1
 cd ..
