288e06e42b60eba019af133002225fd29c44fae2
gammasoft71 <gammasoft71@gmail.com>
2018-03-02 16:39:31 +0100

    test for travis ci linux build 7

--- scripts/travis/build_linux.sh
+++ scripts/travis/build_linux.sh
@@ -4,15 +4,15 @@
 #                                                      generate and build Switch
 pushd build
 cmake .. -DCMAKE_BUILD_TYPE=Debug -DBUILD_SWITCH_SYSTEM_DRAWING=OFF -DBUILD_SWITCH_SYSTEM_WINDOWS_FORMS=OFF -DBUILD_SWITCH_TESTS=ON -DCMAKE_CXX_COMPILER=clang++-3.9
-cmake --build . --target Switch.Core.ManualTests -- -j8
+cmake --build . -- -j8
 if [ $? -ne 0 ]; then exit -1; fi
 popd
 
 # ______________________________________________________________________________
 #                                                      run registered unit tests
 pushd build
-#ctest --output-on-failure --build-config Debug --fixture-exclude RegistryKeyUT
-#if [ $? -ne 0 ]; then exit -1; fi
+ctest --output-on-failure --build-config Debug
+if [ $? -ne 0 ]; then exit -1; fi
 
 #Debug/Switch.Core.UnitTests --gtest_filter=-Registry*.*
 #if [ $? -ne 0 ]; then exit -1; fi
@@ -25,7 +25,4 @@ pushd build
 
 #Debug/Switch.TUnit.UnitTests
 #if [ $? -ne 0 ]; then exit -1; fi
-
-Debug/Switch.Core.ManualTests
-
 popd
