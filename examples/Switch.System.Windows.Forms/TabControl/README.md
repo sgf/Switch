642d76d6b2fd238b8d70a8345ff05deece7d78ce
gammasoft71 <gammasoft71@gmail.com>
2018-03-07 00:58:05 +0100

    add TabControl example

---
+++ examples/Switch.System.Windows.Forms/TabControl/README.md
@@ -0,0 +1,14 @@
+# Panel
+This example demonstrates the use of System::Windows::Forms::Panel control.
+
+![GitHub Logo](../../../docs/Pictures/Examples/Forms/PanelW.png)
+
+Windows
+
+![GitHub Logo](../../../docs/Pictures/Examples/Forms/PanelM.png)
+
+macOS
+
+![GitHub Logo](../../../docs/Pictures/Examples/Forms/PanelU.png)
+
+Ubuntu
---
+++ examples/Switch.System.Windows.Forms/TabControl/TabControl.cpp
@@ -0,0 +1,35 @@
+#include <Switch/Switch>
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+namespace TabControlExample {
+  class Form1 : public Form {
+  public:
+    // The main entry point for the application.
+    static void Main() {
+      Application::EnableVisualStyles();
+      Application::Run(Form1());
+    }
+
+    Form1() {
+      this->tabPage3.Text = "tabPage3";
+      
+      this->tabControl1.Location = System::Drawing::Point(10, 10);
+      this->tabControl1.Size = System::Drawing::Size(350, 250);
+      this->tabControl1.TabPages().Add("tabPage1");
+      this->tabControl1.TabPages().Add("tabPage2", "tabPage2");
+      this->tabControl1.TabPages().Add(this->tabPage3);
+
+      this->ClientSize = System::Drawing::Size(370, 270);
+      this->Text = "TabControl example";
+      this->Controls().Add(this->tabControl1);
+    }
+
+  private:
+    TabControl tabControl1;
+    TabPage tabPage3;
+  };
+}
+
+startup_(TabControlExample::Form1);
