a72b54cea6dde2366fed578de9037f5adff964d5
gammasoft71 <gammasoft71@gmail.com>
2018-03-09 00:29:57 +0100

    fix build

--- src/Switch/include/Switch/Switch.hpp
+++ src/Switch/include/Switch/Switch.hpp
@@ -5,8 +5,6 @@
 #include "Switch/Switch.System.Core"
 #include "Switch/Switch.System.Drawing"
 #include "Switch/Switch.System.ServiceModel"
-#include "Switch/Switch.System.Drawing"
-#include "Switch/Switch.System.ServiceModel"
 #include "Switch/Switch.System.Windows.Forms"
 #include "Switch/Switch.TUnit.Core"
 #include "Switch/Switch.TUnit.Framework"
