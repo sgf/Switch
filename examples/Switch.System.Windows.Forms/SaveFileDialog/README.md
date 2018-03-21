c5aa837646de4fe0baa8cc6f408b8ffb4e93277a
gammasoft71 <gammasoft71@gmail.com>
2018-03-21 22:25:07 +0100

    add SaveFileDialog example

---
+++ examples/Switch.System.Windows.Forms/SaveFileDialog/README.md
@@ -0,0 +1,24 @@
+# SaveFileDialog
+
+This example demonstrates the use of System::Windows::Forms::SaveFileDialog dialog.
+
+![GitHub Logo](../../../docs/Pictures/Examples/Forms/SaveFileDialogW.png)
+
+Windows
+
+![GitHub Logo](../../../docs/Pictures/Examples/Forms/SaveFileDialog1M.png)
+![GitHub Logo](../../../docs/Pictures/Examples/Forms/SaveFileDialog2M.png)
+
+macOS
+
+![GitHub Logo](../../../docs/Pictures/Examples/Forms/SaveFileDialogU.png)
+
+Ubuntu
+
+![GitHub Logo](../../../docs/Pictures/Examples/Forms/SaveFileDialogG.png)
+
+Gnome
+
+![GitHub Logo](../../../docs/Pictures/Examples/Forms/SaveFileDialogGD.png)
+
+Gnome Dark
---
+++ examples/Switch.System.Windows.Forms/SaveFileDialog/SaveFileDialog.cpp
@@ -0,0 +1,42 @@
+#include <Switch/Switch>
+
+using namespace System;
+using namespace System::IO;
+using namespace System::Windows::Forms;
+
+namespace SaveFileDialogExample {
+  class Form1 : public Form {
+  public:
+    // The main entry point for the application.
+    static void Main() {
+      Application::EnableVisualStyles();
+      Application::Run(Form1());
+    }
+
+    Form1() {
+      this->button.Text = "Save...";
+      this->button.Location = System::Drawing::Point(10, 10);
+      this->button.Click += delegate_(const object & sender, const EventArgs & e) {
+        SaveFileDialog saveFileDialog;
+        saveFileDialog.InitialDirectory = Environment::GetFolderPath(Environment::SpecialFolder::Desktop);
+        saveFileDialog.FileName = "MyFile.txt";
+        saveFileDialog.Filter = "Text Files (*.txt)|*.txt;*.md|All Files (*.*)|*.*";
+        if (saveFileDialog.ShowDialog() == DialogResult::OK)
+          this->label.Text = string::Format("File = {0}", saveFileDialog.FileName);
+      };
+
+      this->label.Text = "File = ";
+      this->label.Location = System::Drawing::Point(10, 40);
+      this->label.Width = 340;
+
+      this->Text = "SaveFileDialog example";
+      this->Controls().AddRange({this->button, this->label});
+    }
+
+  private:
+    Button button;
+    Label label;
+  };
+}
+
+startup_(SaveFileDialogExample::Form1);
