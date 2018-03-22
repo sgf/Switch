51535e3ebc930d7a7daa2ebf4b8b02fd102fb131
gammasoft71 <gammasoft71@gmail.com>
2018-03-23 00:59:26 +0100

    add Label example

---
+++ examples/Switch.System.Windows.Forms/Label/Label.cpp
@@ -0,0 +1,41 @@
+#include <Switch/Switch>
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+namespace LabelExample {
+  class Form1 : public Form {
+  public:
+    // The main entry point for the application.
+    static void Main() {
+      Application::EnableVisualStyles();
+      Application::Run(Form1());
+    }
+    
+    Form1() {
+      this->Text = "Label example";
+
+      this->label1.Parent = *this;
+      this->label1.Text = "https://gammasoft71.wixsite.com/gammasoft";
+      this->label1.Location = System::Drawing::Point(10, 10);
+      this->label1.Width = 260;
+      
+      this->label1.MouseEnter += delegate_(const object& sender, const EventArgs& e) {
+        this->label1.ForeColor = System::Drawing::Color::Blue;
+      };
+      
+      this->label1.Click += delegate_(const object& sender, const EventArgs& e) {
+        MessageBox::Show(string::Format("The label \"{0}\" was clicked", this->label1.Text), "Message", MessageBoxButtons::OK);
+      };
+      
+      this->label1.MouseLeave += delegate_(const object& sender, const EventArgs& e) {
+        this->label1.ForeColor = System::Drawing::Color::Black;
+      };
+    }
+    
+  private:
+    Label label1;
+  };
+}
+
+startup_(LabelExample::Form1);
---
+++ examples/Switch.System.Windows.Forms/Label/README.md
@@ -0,0 +1,23 @@
+# Label
+
+This example demonstrates the use of System::Windows::Forms::Label control.
+
+![GitHub Logo](../../../docs/Pictures/Examples/Forms/LabelW.png)
+
+Windows
+
+![GitHub Logo](../../../docs/Pictures/Examples/Forms/LabelM.png)
+
+macOS
+
+![GitHub Logo](../../../docs/Pictures/Examples/Forms/LabelU.png)
+
+Ubuntu
+
+![GitHub Logo](../../../docs/Pictures/Examples/Forms/LabelG.png)
+
+Gnome
+
+![GitHub Logo](../../../docs/Pictures/Examples/Forms/LabelGD.png)
+
+Gnome Dark
