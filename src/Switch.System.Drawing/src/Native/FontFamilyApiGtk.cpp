9d6e01a4a9561b39b09e1146857a09f8e01552cb
gammasoft71 <gammasoft71@gmail.com>
2018-03-28 15:34:36 +0200

    FontFamily implementation on Linux

--- src/Switch.System.Drawing/src/Native/FontFamilyApiGtk.cpp
+++ src/Switch.System.Drawing/src/Native/FontFamilyApiGtk.cpp
@@ -6,22 +6,39 @@
 #undef set_
 
 #include <gtkmm.h>
+#include <glib.h>
+#include <pango/pangocairo.h>
 
 using namespace System;
 using namespace System::Collections::Generic;
 using namespace System::Drawing;
 
+namespace {
+  static PangoFontFamily** pangoFontFamilies = null;
+  static int32 pangoFontFamiliesMax = 0;
+};
+
 Array<System::Drawing::FontFamily> Native::FontFamilyApi::GetInstalledFontFamilies() {
-  System::Array<System::Drawing::FontFamily> families;
-  return families;
+  System::Collections::Generic::List<System::Drawing::FontFamily> families;
+
+  PangoFontMap* fontmap = pango_cairo_font_map_get_default();
+  pango_font_map_list_families (fontmap, &pangoFontFamilies, &pangoFontFamiliesMax);
+  for (int32 index = 0; index < pangoFontFamiliesMax; index++)
+    families.Add(System::Drawing::FontFamily((intptr)index));
+
+  return families.ToArray();
 }
 
 System::Drawing::FontFamily Native::FontFamilyApi::GetFontFamilyFromName(const string& name) {
+  for (int32 index = 0; index < pangoFontFamiliesMax; index++)
+    if (GetName((intptr)index) == name)
+      return System::Drawing::FontFamily((intptr)index);
   throw ArgumentException(caller_);
 }
 
 string Native::FontFamilyApi::GetName(intptr handle) {
-  return "<not implemented>";
+  if ((int32)handle >= pangoFontFamiliesMax) throw InvalidOperationException(caller_);
+  return pango_font_family_get_name (pangoFontFamilies[(int32)handle]);
 }
 
 bool Native::FontFamilyApi::IsStyleAvailable(intptr handle, FontStyle style) {
@@ -36,6 +53,8 @@ bool Native::FontFamilyApi::IsStyleAvailable(intptr handle, FontStyle style) {
 }
 
 void Native::FontFamilyApi::ReleaseResource(intptr handle) {
+  if (pangoFontFamilies != null)
+    g_free (pangoFontFamilies);
 }
 
 #endif
--- tests/Switch.System.Drawing.ManualTests/Main.cpp
+++ tests/Switch.System.Drawing.ManualTests/Main.cpp
@@ -3,14 +3,23 @@
 #include <Switch/Nameof.hpp>
 #include <Switch/Startup.hpp>
 #include <Switch/System/Console.hpp>
+#include <Switch/System/Drawing/FontFamily.hpp>
 
 using namespace System;
+using namespace System::Drawing;
 
 namespace ManualTests {
   class Program {
   public:
     // The main entry point for the application.
     static void Main() {
+      //Console::WriteLine("GenericMonospacef : {0}", FontFamily::GenericMonospace().Name);
+      //Console::WriteLine("GenericSansSerif : {0}", FontFamily::GenericSansSerif().Name);
+      Console::WriteLine("GenericSerif : {0}", FontFamily::GenericSerif().Name);
+      Console::WriteLine("Families : ");
+      for (FontFamily fontFamily : FontFamily::Families()) {
+        Console::WriteLine("  fontFamily = {0}", fontFamily.Name);
+      }
     }
   };
 }
