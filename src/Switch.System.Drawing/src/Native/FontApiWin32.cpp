e9ac29b9ed2f7ec0c0f08bdd768c0a48b104f52c
gammasoft71 <gammasoft71@gmail.com>
2018-04-01 14:48:12 +0200

    Font and SystemFonts imùplementation

--- src/Switch.System.Drawing/src/Native/FontApiWin32.cpp
+++ src/Switch.System.Drawing/src/Native/FontApiWin32.cpp
@@ -13,7 +13,7 @@ using namespace System;
 using namespace System::Drawing;
 
 intptr Native::FontApi::Create(const System::Drawing::FontFamily& family, float emSize, System::Drawing::FontStyle style, byte gdiCharSet, bool gdiVerticalFont) {
-  return (intptr)CreateFont((int32)-GetHeight(emSize), 0, 0, 0, (style & FontStyle::Bold) == FontStyle::Bold ? FW_BOLD : FW_DONTCARE, (style & FontStyle::Italic) == FontStyle::Italic ? TRUE : FALSE, (style & FontStyle::Underline) == FontStyle::Underline ? TRUE : FALSE, (style & FontStyle::Strikeout) == FontStyle::Strikeout ? TRUE : FALSE, gdiCharSet, OUT_OUTLINE_PRECIS, CLIP_DEFAULT_PRECIS, CLEARTYPE_QUALITY, DEFAULT_PITCH | FF_DONTCARE, family.Name().w_str().c_str());
+  return (intptr)CreateFont((int32) - GetHeight(emSize), 0, 0, 0, (style & FontStyle::Bold) == FontStyle::Bold ? FW_BOLD : FW_DONTCARE, (style & FontStyle::Italic) == FontStyle::Italic ? TRUE : FALSE, (style & FontStyle::Underline) == FontStyle::Underline ? TRUE : FALSE, (style & FontStyle::Strikeout) == FontStyle::Strikeout ? TRUE : FALSE, gdiCharSet, OUT_OUTLINE_PRECIS, CLIP_DEFAULT_PRECIS, CLEARTYPE_QUALITY, DEFAULT_PITCH | FF_DONTCARE, family.Name().w_str().c_str());
 }
 
 void Native::FontApi::Destroy(intptr hfont) {
