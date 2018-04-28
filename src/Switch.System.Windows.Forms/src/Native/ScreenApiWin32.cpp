6207c256666fb563a9a607612ef3f47429e507b2
gammasoft71 <gammasoft71@gmail.com>
2018-04-28 20:52:38 +0200

    Fix DEviceName

--- src/Switch.System.Windows.Forms/src/Native/ScreenApiWin32.cpp
+++ src/Switch.System.Windows.Forms/src/Native/ScreenApiWin32.cpp
@@ -20,7 +20,6 @@ struct Win32Screen : public object {
   System::Drawing::Rectangle workingArea;
 };
 
-int screenNumber = 0;
 static BOOL CALLBACK ScreenCallback(HMONITOR monitor, HDC handle, LPRECT rect, LPARAM data) {
   List<Win32Screen>* win32Screens = (List<Win32Screen>*)data;
   MONITORINFOEX monitorInfo;
@@ -29,7 +28,7 @@ static BOOL CALLBACK ScreenCallback(HMONITOR monitor, HDC handle, LPRECT rect, L
   Win32Screen win32Screen;
   win32Screen.bitsPerPixel = 32;
   win32Screen.bounds = { monitorInfo.rcMonitor.left, monitorInfo.rcMonitor.top, monitorInfo.rcMonitor.right - monitorInfo.rcMonitor.left, monitorInfo.rcMonitor.bottom - monitorInfo.rcMonitor.top };
-  win32Screen.deviceName = string::Format("\\\\.\\DISPLAY{0}", ++screenNumber);
+  win32Screen.deviceName = monitorInfo.szDevice;
   win32Screen.primary = win32Screen.deviceName == "\\\\.\\DISPLAY1";
   win32Screen.workingArea = { monitorInfo.rcWork.left, monitorInfo.rcWork.top, monitorInfo.rcWork.right - monitorInfo.rcWork.left, monitorInfo.rcWork.bottom - monitorInfo.rcWork.top };
   win32Screens->Add(win32Screen);
@@ -39,7 +38,6 @@ static BOOL CALLBACK ScreenCallback(HMONITOR monitor, HDC handle, LPRECT rect, L
 Array<Screen> __screens__::__get_screens__() {
   List<Screen> screens;
   List<Win32Screen> win32Screens;
-  screenNumber = 0;
   EnumDisplayMonitors(null, null, ScreenCallback, (LPARAM)&win32Screens);
   for (Win32Screen win32Screen : win32Screens) {
     Screen screen;
