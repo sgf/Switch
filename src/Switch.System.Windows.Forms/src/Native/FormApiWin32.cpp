3c48945f8a826341602a1e7a1eff576f76c1cddb
gammasoft71 <gammasoft71@gmail.com>
2018-04-28 20:54:45 +0200

    Fix FormStartPosition with ClientSize

--- src/Switch.System.Windows.Forms/src/Native/FormApiWin32.cpp
+++ src/Switch.System.Windows.Forms/src/Native/FormApiWin32.cpp
@@ -32,10 +32,20 @@ intptr Native::FormApi::Create(System::Windows::Forms::Form& form, bool withClie
   HWND handle = CreateWindowEx(0, WC_DIALOG, form.Text().w_str().c_str(), WS_OVERLAPPED | WS_CAPTION | WS_CLIPSIBLINGS | WS_SYSMENU | WS_THICKFRAME | WS_MINIMIZEBOX | WS_MAXIMIZEBOX | WS_GROUP, bounds.Left, bounds.Top, bounds.Width, bounds.Height, NULL, (HMENU)0, GetModuleHandle(NULL), (LPVOID)NULL);
   WindowProcedure::DefWindowProcs[(intptr)handle] = (WNDPROC)SetWindowLongPtr(handle, GWLP_WNDPROC, (LONG_PTR)WindowProcedure::WndProc);
 
-  RECT rect = { 0, 0, 0, 0 };
-  GetWindowRect(handle, &rect);
-  form.Location = System::Drawing::Point(rect.left, rect.top);
-  form.Size = System::Drawing::Size(rect.right - rect.left, rect.bottom - rect.top);
+  if (withClientsize && form.StartPosition == FormStartPosition::CenterScreen) {
+    RECT rect = { 0, 0, form.ClientSize().Width, form.ClientSize().Height };
+    AdjustWindowRectEx(&rect, GetWindowLong(handle, GWL_STYLE), false /*menu == null*/, GetWindowLong(handle, GWL_EXSTYLE));
+    form.Size = System::Drawing::Size(rect.right - rect.left, rect.bottom - rect.top);
+    bounds = System::Drawing::Rectangle((Screen::AllScreens()[0].WorkingArea().Width + Screen::AllScreens()[0].WorkingArea().X - form.Width) / 2, (Screen::AllScreens()[0].WorkingArea().Height + Screen::AllScreens()[0].WorkingArea().Y - form.Height) / 2, form.Width, form.Height);
+    form.Location = System::Drawing::Point(bounds.Left, bounds.Top);
+    form.Size = System::Drawing::Size(bounds.Width, bounds.Height);
+  } else {
+    RECT rect = { 0, 0, 0, 0 };
+    GetWindowRect(handle, &rect);
+    form.Location = System::Drawing::Point(rect.left, rect.top);
+    form.Size = System::Drawing::Size(rect.right - rect.left, rect.bottom - rect.top);
+  }
+
   return (intptr)handle;
 }
 
