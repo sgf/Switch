42ebec0591cb67b56894ca3e7a3b7697e49e9d85
gammasoft71 <gammasoft71@gmail.com>
2018-05-03 14:43:52 +0200

    add ApplicationAndMain example

---
+++ examples/Switch.System.Windows.Forms/ApplicationAndMain/ApplicationAndMain.cpp
@@ -0,0 +1,26 @@
+#include <Switch/Switch>
+
+using namespace System::Windows::Forms;
+
+namespace Examples {
+  class Program {
+  public:
+    static void Main() {
+      Application::Run(Form());
+    }
+  };
+}
+
+// You can write following lines instead "startup_(Examples::Program);" helper call :
+
+// The main entry point for the application.
+int32 main(int argc, char* argv[]) {
+  System::Environment::SetCommandLineArgs(argv, argc);
+  Examples::Program::Main();
+  return System::Environment::ExitCode;
+}
+
+// The main entry point for the Microsoft Windows application (this mathod has not effect on other Operating System).
+int __stdcall WinMain(HINSTANCE instance, HINSTANCE previousInstance, char* commandLine, int commandShow) {
+  return main(__argc, __argv);
+}
---
+++ examples/Switch.System.Windows.Forms/ApplicationAndMain/README.md
@@ -0,0 +1,23 @@
+# Application
+
+This example demonstrates the use of System::Windows::Forms::Application class.
+
+Windows :
+
+![GitHub Logo](../../../docs/Pictures/Examples/Forms/ApplicationW.png)
+
+macOS :
+
+![GitHub Logo](../../../docs/Pictures/Examples/Forms/ApplicationM.png)
+
+Ubuntu :
+
+![GitHub Logo](../../../docs/Pictures/Examples/Forms/ApplicationU.png)
+
+Gnome :
+
+![GitHub Logo](../../../docs/Pictures/Examples/Forms/ApplicationG.png)
+
+Gnome Dark :
+
+![GitHub Logo](../../../docs/Pictures/Examples/Forms/ApplicationGD.png)
