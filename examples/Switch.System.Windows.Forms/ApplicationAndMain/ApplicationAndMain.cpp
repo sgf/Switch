67beb583aceaa6d1c40588eb05a749040ca267ad
Gammasoft <gammasoft71@gmail.com>
2018-05-28 00:19:12 +0200

    Update ApplicationAndMain.cpp

--- examples/Switch.System.Windows.Forms/ApplicationAndMain/ApplicationAndMain.cpp
+++ examples/Switch.System.Windows.Forms/ApplicationAndMain/ApplicationAndMain.cpp
@@ -1,7 +1,7 @@
 #include <Switch/Switch>
 
 // The main entry point for the application.
-int32 main(int argc, char* argv[]) {
+int main(int argc, char* argv[]) {
   System::Environment::SetCommandLineArgs(argv, argc);
   System::Windows::Forms::Application::Run(System::Windows::Forms::Form());
 }
