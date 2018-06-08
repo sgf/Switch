a94bbb76e39bcd3c448b98cd2ab3beddb75ba8ae
gammasoft71 <gammasoft71@gmail.com>
2018-06-08 21:23:01 +0200

    review code

--- examples/Switch.System.Windows.Forms/TimerForm/TimerForm.cpp
+++ examples/Switch.System.Windows.Forms/TimerForm/TimerForm.cpp
@@ -13,16 +13,9 @@ namespace TimerFormExample {
     }
 
     Form1() {
-      this->Text = "Timer Example";
-      this->StartPosition = FormStartPosition::Manual;
-      this->Location = System::Drawing::Point(400, 300);
-      this->ClientSize = System::Drawing::Size(230, 130);
-
-      this->label.Parent = *this;
-      this->label.Text = string::Format("{0:N1}", as<double>(this->counter) / 10);
+      this->label.Text = string::Format("{0:F1}", as<double>(this->counter) / 10);
       this->label.Location = System::Drawing::Point(10, 10);
       this->label.Size = System::Drawing::Size(210, 70);
-      //this->label.AutoSize = true;
       this->label.Font = System::Drawing::Font(this->label.Font().Name, 48, System::Drawing::FontStyle::Italic);
       this->label.ForeColor = System::Drawing::Color::DodgerBlue;
 
@@ -31,14 +24,18 @@ namespace TimerFormExample {
         this->label.Text = string::Format("{0:F1}", as<double>(++this->counter) / 10);
       };
 
-      this->button.Parent = *this;
       this->button.Text = "Start";
       this->button.Location = System::Drawing::Point(10, 90);
-
       this->button.Click += delegate_(const object & sender, const EventArgs & e) {
         this->timer.Enabled = !this->timer.Enabled;
         this->button.Text = this->timer.Enabled ? "Stop" : "Start";
       };
+
+      this->Text = "Timer Example";
+      this->StartPosition = FormStartPosition::Manual;
+      this->Location = System::Drawing::Point(400, 300);
+      this->ClientSize = System::Drawing::Size(230, 130);
+      this->Controls().AddRange({this->label, this->button});
     }
 
   private:
--- examples/Switch.System.Windows.Forms/TrackBar/TrackBar.cpp
+++ examples/Switch.System.Windows.Forms/TrackBar/TrackBar.cpp
@@ -18,22 +18,20 @@ namespace TrackBarExample {
       this->trackBar.Parent = *this;
       this->trackBar.Location = System::Drawing::Point(20, 50);
       this->trackBar.Maximum = 200;
-      this->trackBar.Value = 100;
       this->trackBar.Width = 200;
       this->trackBar.TickStyle = TickStyle::None;
       this->trackBar.ValueChanged += delegate_(const object & sender, const EventArgs & e) {
         this->progressBar.Value = this->trackBar.Value;
         this->label.Text = string::Format("{0}", this->trackBar.Value);
       };
+      this->trackBar.Value = 100;
 
       this->progressBar.Parent = *this;
       this->progressBar.Location = System::Drawing::Point(20, 100);
       this->progressBar.Maximum = 200;
-      this->progressBar.Value = this->trackBar.Value;
       this->progressBar.Width = 200;
 
       this->label.Parent = *this;
-      this->label.Text = string::Format("{0}", this->trackBar.Value);
       this->label.Location = System::Drawing::Point(20, 150);
     }
 
